c329ad5410d80f98ae2f6030399c1b9542f855f9
Kwonjooeun <70623530+Kwonjooeun@users.noreply.github.com>
2025-06-12 16:19:41 +0900

    Update CommonTypes.h

--- Common/Types/CommonTypes.h
+++ Common/Types/CommonTypes.h
@@ -180,5 +180,34 @@ inline std::string SetCmdToString(EN_SET_CMD cmd) {
         default: return "UNKNOWN";
     }
 }
+struct SPOINT_ENU
+{
+    double E;
+    double N;
+    double U;
+};
 
+struct SPOINT_M_MINE_ENU
+{
+    double E;
+    double N;
+    double U;
+    double Speed;
+    bool Validation;
+};
+
+struct GEO_POINT_2D
+{
+    double latitude;
+    double longitude;
+};
+
+struct SAIEP_WAYPOINTS
+{
+    int TubeNumber;
+    int count_waypoints = 0;
+    double arrE[15];
+    double arrN[15];
+    double arrU[15];
+};
 } // namespace WeaponControl
