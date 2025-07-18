561d7685c31572dcc1155e6f381a42ca2b7b86a3
Kwonjooeun <70623530+Kwonjooeun@users.noreply.github.com>
2025-07-18 14:21:10 +0900

    Update M_MINE_DroppingPlanManager.h

--- EngagementPlanningFactory/EngagementManagers/M_MINE/M_MINE_DroppingPlanManager/M_MINE_DroppingPlanManager.h
+++ EngagementPlanningFactory/EngagementManagers/M_MINE/M_MINE_DroppingPlanManager/M_MINE_DroppingPlanManager.h
@@ -1,59 +1,61 @@
 #pragma once
 
-#include "../../../dds_message/AIEP_AIEP_.hpp"
-#include "../../../Common/Utils/ConfigManager.h"
-#include "../EngagementManagerBase.h"
-#include "M_MINE_DroppingPlanManager/M_Mine_DroppingPlanManager.h"
-#include "M_MINE_Model/M_MINE_Dynamics.h"
-#include <memory>
-#include <vector>
-#include <chrono>
+#include <fstream>
+#include <cstring>
+#include <map>
+#include <array>
+#include <iostream>
+#include <thread>
+#include <cassert>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
 
-namespace MINEASMALM {
-    // =============================================================================
-    // 자항기뢰 전용 교전계획 관리자
-    // =============================================================================
-    class MineEngagementManager : public EngagementManagerBase {
-    public:
-        MineEngagementManager(WeaponAssignmentInfo weaponAssignInfo, std::shared_ptr<DdsComm> ddsComm);
-        ~MineEngagementManager() = default;
+#define NOMINMAX
+#include "../../../../dds_message/AIEP_AIEP_.hpp"
+#include "../../../../dds_library/dds.h"
+#include "json.hpp"
+//#include "AIEP_Defines.h"
+#undef NOMINMAX  // now std::min/std::max work again
+namespace MINEASMALM{
+using json = nlohmann::json;
 
-        // ==========================================================================
-        // 부설계획 관리
-        // ==========================================================================
-        bool SetMineDropPlan(uint32_t listNum, uint32_t planNum);
-        bool UpdateDropPlanWaypoints(const std::vector<ST_WEAPON_WAYPOINT>& waypoints);
-        bool GetDropPlan(ST_M_MINE_PLAN_INFO& planInfo) const;
+class M_MineDroppingPlanManager
+{
+public:
+	json pointToJson(const ST_WEAPON_WAYPOINT& pt);
+	json waypointToJson(const ST_M_MINE_PLAN_OWNSHIP_WAYPOINT& wpt);
+	json createEmptyPlanJson();
+	void ensure_json_file_exists(const std::string& filename);
+	json ddsMessageToJson(const CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg);
+	bool saveDDSMessageToJsonFile(const CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg, const std::string& filename);
+	void jsonToDdsMessage(const json& j, AIEP_CMSHCI_M_MINE_ALL_PLAN_LIST& msg);
+	void jsonToDdsMessage(const json& j, CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg);
+	bool loadPlanFromFile(AIEP_CMSHCI_M_MINE_ALL_PLAN_LIST& msg, const std::string& filename);
+	bool loadPlanFromFile(CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg, const std::string& filename);
+	bool readDroppingPlanfromFile(const std::string& filename, int DroppingPlanListIdx, int DroppingPlanIdx, ST_M_MINE_PLAN_INFO& DropPlan);
+	bool savePlanToFile(const CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg, const std::string& filename);
+	bool readDropPosFromJson(
+		const std::string& filePath,
+		int planListIndex,
+		int planIndex,
+		double& outLatitude,
+		double& outLongitude,
+		float& outDepth,
+		float& outSpeed,
+		bool& outValid
+	);
+	bool readLaunchPosFromJson(
+		const std::string& filePath,
+		int planListIndex,
+		int planIndex,
+		double& outLatitude,
+		double& outLongitude,
+		float& outDepth,
+		float& outSpeed,
+		bool& outValid
+	);
 
-        uint32_t GetDropPlanListNumber() const;
-        uint32_t GetDropPlanNumber() const;
-        bool IsDropPlanLoaded() const;
+};
+}
 
-    protected:
-        // EngagementManagerBase 구현
-        void UpdateEngagementResult() override;
-        void SendEngagementResult() override;
-
-        void PlanTrajectory() override;
-        void EstimateCurrentStatus() override;
-    private:
-
-        M_MineDroppingPlanManager* DroppingPlanManager;
-        
-        // 부설계획 정보
-        ST_M_MINE_PLAN_INFO m_dropPlan;
-        bool m_dropPlanLoaded{ false };
-        uint32_t m_dropPlanListNumber{ 0 };
-        uint32_t m_dropPlanNumber{ 0 };
-        mutable std::mutex m_planMutex;
-
-        // 자항기뢰 위치 계산
-        ST_3D_GEODETIC_POSITION CalculateMinePosition(float timeSinceLaunch) const;
-
-        const std::string MINE_PLAN_FILE = "Hello.json";
-
-        // 부설 계획 검증
-        bool ValidateDropPlan() const;
-    };
-
-} // namespace AIEP
