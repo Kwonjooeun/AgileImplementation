82a4f84e0e4a309774bb5b967e39ea6d6a37c3dc
Kwonjooeun <70623530+Kwonjooeun@users.noreply.github.com>
2025-09-05 13:21:45 +0900

    Update TubeMessageReceiver.cpp

--- TubeMessageReceiver.cpp
+++ TubeMessageReceiver.cpp
@@ -1,14 +1,16 @@
 #include "TubeMessageReceiver.h"
 #include <iostream>
 
-namespace MINEASMALM {
-    TubeMessageReceiver::TubeMessageReceiver(int tubeNumber, LaunchTubeManager* launchtubemanager, std::shared_ptr<DdsComm> ddsComm)
+namespace AIEP {
+    TubeMessageReceiver::TubeMessageReceiver(int tubeNumber, LaunchTubeManager* launchtubemanager, std::shared_ptr<AIEP::DdsComm> ddsComm)
         : m_tubeNumber(tubeNumber)
         , m_launchtubemanager(launchtubemanager)
         , m_ddsComm(ddsComm)
         , m_initialized(false)
         , m_shutdown(false)
-    {}
+    {
+        Initialize();
+    }
 
     TubeMessageReceiver::~TubeMessageReceiver() {
         Shutdown();
@@ -25,28 +27,36 @@ namespace MINEASMALM {
         }
 
         try {
-
             DEBUG_STREAM(MESSAGERECEIVER) << "Initializing TubeMessageHandler for Tube " << m_tubeNumber << std::endl;
-            
+
             // 적재정보 콜백 등록
             m_ddsComm->RegisterReader<TEWA_WA_TUBE_LOAD_INFO>(
                 [this](const TEWA_WA_TUBE_LOAD_INFO& msg) { OnLoadInfoReceived(msg); });
-            
+
             // DDS Reader 콜백 등록
             m_ddsComm->RegisterReader<TEWA_ASSIGN_CMD>(
                 [this](const TEWA_ASSIGN_CMD& msg) { OnAssignCommandReceived(msg); });
 
+            m_ddsComm->RegisterReader<CMSHCI_AIEP_PA_INFO>(
+                [this](const CMSHCI_AIEP_PA_INFO& msg) { OnPAInfoReceived(msg); });
+
             m_ddsComm->RegisterReader<CMSHCI_AIEP_WPN_CTRL_CMD>(
                 [this](const CMSHCI_AIEP_WPN_CTRL_CMD& msg) { OnWeaponControlCommandReceived(msg); });
 
-            //m_ddsComm->RegisterReader<CMSHCI_AIEP_WPN_GEO_WAYPOINTS>(
-            //    [this](const CMSHCI_AIEP_WPN_GEO_WAYPOINTS& msg) { OnWaypointsReceived(msg); });
+            m_ddsComm->RegisterReader<CMSHCI_AIEP_WPN_GEO_WAYPOINTS>(
+                [this](const CMSHCI_AIEP_WPN_GEO_WAYPOINTS& msg) { OnWaypointsReceived(msg); });
 
-            //m_ddsComm->RegisterReader<NAVINF_SHIP_NAVIGATION_INFO>(
-            //    [this](const NAVINF_SHIP_NAVIGATION_INFO& msg) { OnOwnShipInfoReceived(msg); });
+            m_ddsComm->RegisterReader<NAVINF_SHIP_NAVIGATION_INFO>(
+                [this](const NAVINF_SHIP_NAVIGATION_INFO& msg) { OnOwnShipInfoReceived(msg); });
 
-            //m_ddsComm->RegisterReader<TRKMGR_SYSTEMTARGET_INFO>(
-            //    [this](const TRKMGR_SYSTEMTARGET_INFO& msg) { OnTargetInfoReceived(msg); });
+            m_ddsComm->RegisterReader<AIEP_WPN_CTRL_STATUS_INFO>(
+                [this](const AIEP_WPN_CTRL_STATUS_INFO& msg) { OnTargetInfoReceived(msg); });
+
+            m_ddsComm->RegisterReader<CMSHCI_AIEP_AI_WAYPOINTS_INFERENCE_REQ>(
+                [this](const CMSHCI_AIEP_AI_WAYPOINTS_INFERENCE_REQ& msg) { OnAIWaypointsInferenceRequestReceived(msg); });
+
+            m_ddsComm->RegisterReader<AIEP_INTERNAL_INFER_RESULT_WP>(
+                [this](const AIEP_INTERNAL_INFER_RESULT_WP& msg) { OnAIWaypointsInferenceResultReceived(msg); });
 
             //m_ddsComm->RegisterReader<CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST>(
             //    [this](const CMSHCI_AIEP_M_MINE_EDITED_PLAN_LIST& msg) { OnEditedPlanListReceived(msg); });
@@ -76,7 +86,6 @@ namespace MINEASMALM {
         DEBUG_STREAM(MESSAGERECEIVER) << "TubeMessageHandler for Tube " << m_tubeNumber << " shutdown completed" << std::endl;
     }
 
-    // 유효성 검사 함수들
     bool TubeMessageReceiver::IsMessageForThisTube(int messageTargetTube) const {
         return messageTargetTube == m_tubeNumber;
     }
@@ -84,18 +93,16 @@ namespace MINEASMALM {
     void TubeMessageReceiver::OnLoadInfoReceived(const TEWA_WA_TUBE_LOAD_INFO& message)
     {
         int targetTube = static_cast<int>(message.eTubeNum());
-        // 발사관 번호 확인
-        if (IsMessageForThisTube(targetTube)) {
+
+        if (!IsMessageForThisTube(targetTube)) {
             return;
         }
 
-        // 무장 종류만 업데이트 (한 줄 처리)
         m_launchtubemanager->UpdateLoadedWeaponKind(
             static_cast<EN_WPN_KIND>(message.eWpnKind())
         );
     }
 
-    // DDS 메시지 수신 콜백 구현
     void TubeMessageReceiver::OnAssignCommandReceived(const TEWA_ASSIGN_CMD& message) {
         int targetTube = static_cast<int>(message.stWpnAssign().enTubeNum());
 
@@ -105,10 +112,10 @@ namespace MINEASMALM {
 
         DEBUG_STREAM(MESSAGERECEIVER) << "Tube " << m_tubeNumber << " received TEWA_ASSIGN_CMD" << std::endl;
 
-        bool success = false;
+        bool success{ false };
 
         try {
-            if (message.eSetCmd() == static_cast<uint32_t>(EN_SET_CMD::SET_CMD_SET)) {
+           if (message.eSetCmd() == static_cast<uint32_t>(EN_SET_CMD::SET_CMD_SET)) {
                 success = m_launchtubemanager->AssignWeapon(message);
                 if (success) {
                     AIEP_ASSIGN_RESP RespMsg;
@@ -120,7 +127,12 @@ namespace MINEASMALM {
                 }
                 else
                 {
-                    DEBUG_ERROR_STREAM(MESSAGERECEIVER) << "Failed to assign weapon" << std::endl;
+                    AIEP_ASSIGN_RESP RespMsg;
+                    RespMsg.eSetCmd() = static_cast<uint32_t>(EN_SET_CMD::SET_CMD_REJECTED);
+                    RespMsg.stWpnAssign() = message.stWpnAssign();
+
+                    m_ddsComm->Send(RespMsg);
+                    DEBUG_ERROR_STREAM(MESSAGERECEIVER) << "Rejected to assign weapon" << std::endl;
                 }
             }
             else if (message.eSetCmd() == static_cast<uint32_t>(EN_SET_CMD::SET_CMD_UNSET)) {
@@ -135,7 +147,12 @@ namespace MINEASMALM {
                 }
                 else
                 {
-                    DEBUG_ERROR_STREAM(MESSAGERECEIVER) << "Failed to unassign weapon" << std::endl;
+                    AIEP_ASSIGN_RESP RespMsg;
+                    RespMsg.eSetCmd() = static_cast<uint32_t>(EN_SET_CMD::SET_CMD_REJECTED);
+                    RespMsg.stWpnAssign() = message.stWpnAssign();
+
+                    m_ddsComm->Send(RespMsg);
+                    DEBUG_ERROR_STREAM(MESSAGERECEIVER) << "Rejected to unassign weapon" << std::endl;
                 }
             }
             else {
@@ -147,6 +164,11 @@ namespace MINEASMALM {
         }
     }
 
+    void TubeMessageReceiver::OnPAInfoReceived(const CMSHCI_AIEP_PA_INFO& message)
+    {
+        m_launchtubemanager->ProcessPAInfo(message);
+    }
+
     void TubeMessageReceiver::OnWeaponControlCommandReceived(const CMSHCI_AIEP_WPN_CTRL_CMD& message)
     {
         int targetTube = static_cast<int>(message.eTubeNum());
@@ -156,4 +178,46 @@ namespace MINEASMALM {
         }
         m_launchtubemanager->ProcessWeaponControlCommand(message);
     }
-} // namespace MINEASMALM
+
+    void TubeMessageReceiver::OnWaypointsReceived(const CMSHCI_AIEP_WPN_GEO_WAYPOINTS& message)
+    {
+        int targetTube = static_cast<int>(message.eTubeNum());
+
+        if (!IsMessageForThisTube(targetTube)) {
+            return;
+        }
+        m_launchtubemanager->ProcessWaypointCommand(message);
+    }
+
+    void TubeMessageReceiver::OnOwnShipInfoReceived(const NAVINF_SHIP_NAVIGATION_INFO& message)
+    {
+        m_launchtubemanager->ProcessOwnshipInfo(message);
+    }
+
+    void TubeMessageReceiver::OnTargetInfoReceived(const AIEP_WPN_CTRL_STATUS_INFO& message)
+    {
+        int a = 0;
+        //m_launchtubemanager->ProcessSystemTargetInfo(message);
+    }
+
+    void TubeMessageReceiver::OnAIWaypointsInferenceRequestReceived(const CMSHCI_AIEP_AI_WAYPOINTS_INFERENCE_REQ& message)
+    {
+        int targetTube = static_cast<int>(message.eTubeNum());
+
+        if (!IsMessageForThisTube(targetTube)) {
+            return;
+        }
+        m_launchtubemanager->ProcessAIWaypointsInferenceRequest(message);
+    }
+
+
+    void TubeMessageReceiver::OnAIWaypointsInferenceResultReceived(const AIEP_INTERNAL_INFER_RESULT_WP& message)
+    {
+        int targetTube = static_cast<int>(message.enTubeNum());
+
+        if (!IsMessageForThisTube(targetTube)) {
+            return;
+        }
+        m_launchtubemanager->ProcessAIWaypointsInferenceResult(message);
+    }
+} // namespace AIEP
