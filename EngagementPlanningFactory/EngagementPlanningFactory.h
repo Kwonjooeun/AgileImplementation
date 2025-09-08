3ce36a146f099115cfb68af87afc8222403a52d2
Kwonjooeun <70623530+Kwonjooeun@users.noreply.github.com>
2025-09-08 10:56:59 +0900

    Update EngagementPlanningFactory.h

--- EngagementPlanningFactory/EngagementPlanningFactory.h
+++ EngagementPlanningFactory/EngagementPlanningFactory.h
@@ -1,10 +1,12 @@
+#pragma once
+
 #include <memory>
 #include <functional>
 #include <map>
 
 #include "EngagementManagers/IEngagementManager.h"
 
-namespace MINEASMALM {
+namespace AIEP {
 	class EngagementPlanningFactory
 	{
 	public:
@@ -13,9 +15,23 @@ namespace MINEASMALM {
 
         // 교전계획 관리자 생성
         std::unique_ptr<IEngagementManager> CreateEngagementManager(
-            WeaponAssignmentInfo weaponAssignInfo,
-            std::shared_ptr<DdsComm> ddsComm
+            ST_WA_SESSION weaponAssignInfo,
+            std::shared_ptr<AIEP::DdsComm> ddsComm
         );
+
+        /**
+         * @brief 무장 지원 여부 확인
+         * @param weaponKind 무장 종류
+         * @return 지원 여부
+         */
+        //bool IsWeaponKindSupported(EN_WPN_KIND weaponKind);
+
+        /**
+         * @brief 무장 종류 이름 반환
+         * @param weaponKind 무장 종류
+         * @return 무장 종류 이름
+         */
+        //std::string GetWeaponTypeName(EN_WPN_KIND weaponKind);
 
     private:
         // Singleton 패턴
@@ -26,9 +42,13 @@ namespace MINEASMALM {
 
         // 내부 팩토리 메서드들
         std::unique_ptr<IEngagementManager> CreateMineEngagementManager(
-            WeaponAssignmentInfo weaponAssignInfo, std::shared_ptr<DdsComm> ddsComm);
+            ST_WA_SESSION weaponAssignInfo, std::shared_ptr<AIEP::DdsComm> ddsComm);
 
-        bool IsWeaponKindSupported(EN_WPN_KIND weaponKind) const;
+        std::unique_ptr<IEngagementManager> CreateMissileEngagementManager(
+            ST_WA_SESSION weaponAssignInfo, std::shared_ptr<AIEP::DdsComm> ddsComm);
+
+        //static const std::set<EN_WPN_KIND> s_supportedWeaponKinds;
+        bool IsWeaponKindSupported(uint32_t weaponKind) const;
 
     };
 } // namespace AIEP
